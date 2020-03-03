674cf44e77a1f09210e8ed0b1462ee491abefbcd
dirkmittmann <dirkmittmann@gmail.com>
2020-03-03 16:56:26 +0100

    HelloReact! Komponente mit verbesserter Ausgabe, Material-UI

--- src/hello-react.tsx
+++ src/hello-react.tsx
@@ -1,13 +1,36 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { Typography, CssBaseline } from '@material-ui/core';
+
+const useStyles = makeStyles(() =>
+    createStyles({
+    root: {
+        maxWidth: 500,
+        marginTop: 20,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        backgroundColor: '#e6e2d3',
+        textAlign: 'center',
+    }
+})
+);
 
 const HelloReact = () => {
+    const classes = useStyles();
     return (
-        <h1>Hello React!</h1>
+        <div className={classes.root}>
+            <Typography variant="h2">
+                Hello React!
+            </Typography>
+        </div>
     );
 }
 
 ReactDOM.render(
-    <HelloReact />,
+    <React.Fragment>
+        <CssBaseline />
+        <HelloReact />
+    </React.Fragment>,
     document.querySelector('#root')
 );
